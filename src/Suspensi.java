09693ec6783ae0fd129b4577c388f3894ab5a6c6
MEUTIA KHANANDIYA <MEUTIA KHANANDIYA@DESKTOP-VCH5KKE>
2018-11-04 18:51:57 +0700

    Menambahkan Class Suspensi pada project

---
+++ src/Suspensi.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MEUTIA KHANANDIYA
+ */
+public class Suspensi {
+
+    private String idSuspensi, jenisSuspensi, merkSuspensi;
+    private Mobil idRangka;
+
+    public Suspensi(String id, String jenis, String merk, Mobil idRang) {
+        idSuspensi = id;
+        jenisSuspensi = jenis;
+        merkSuspensi = merk;
+        idRangka = idRang;
+    }
+
+    public void setIDSuspensi(String id) {
+        idSuspensi = id;
+    }
+
+    public String getIDSuspensi() {
+        return idSuspensi;
+    }
+
+    public void setJenisSuspensi(String jenis) {
+        jenisSuspensi = jenis;
+    }
+
+    public String getJenisSuspensi() {
+        return jenisSuspensi;
+    }
+
+    public void setMerkSuspensi(String merk) {
+        merkSuspensi = merk;
+    }
+
+    public String getMerkSuspensi() {
+        return merkSuspensi;
+    }
+
+    public void setIDRangka(Mobil idRang) {
+        idRangka = idRang;
+    }
+
+    public Mobil getIDRangka() {
+        return idRangka;
+    }
+
+    public void info() {
+        System.out.println("Data Suspensi");
+        System.out.println("");
+        System.out.println("No.Seri Suspensi : " + getIDSuspensi());
+        System.out.println("Jenis Suspensi : " + getJenisSuspensi());
+        System.out.println("Merk Suspensi : " + getMerkSuspensi());
+        System.out.println("Suspensi Untuk Rangka : " + idRangka.getIDRangka());
+    }
+}
